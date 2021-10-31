080a30f47985e159138889021856b6c5cd9b7d1f
Patrick Thach <thachp@gmail.com>
2021-10-31 05:36:36 -0700

    Rebuilt components.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,7 @@
 export * from "./enums/componentMode.enum";
 export * from "./enums/componentType.enum";
+export * from "./interfaces/pagecomponentpresenter.interface";
+export * from "./interfaces/linkitem.interface";
+export * from "./pagecomponents/default-header/Index";
+export * from "./pagecomponents/default-section/Index";
+export * from "./pagecomponents/default-footer/Index";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export * from "./enums/componentMode.enum";
 export * from "./enums/componentType.enum";
 
+export * from "./interfaces/pagecomponentpresenter.interface";
+export * from "./interfaces/linkitem.interface";
+
+export * from "./pagecomponents/default-header/Index"
+export * from "./pagecomponents/default-section/Index"
+export * from "./pagecomponents/default-footer/Index"
